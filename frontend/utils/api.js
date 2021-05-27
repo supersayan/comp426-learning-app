e4e5c64f162147d73416b8c28d65dde361c105d7
supersayan <sayandutta135@gmail.com>
2021-05-27 02:19:39 -0400

    update env

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -2,7 +2,7 @@ import { getSession } from 'next-auth/client';
 
 export function getStrapiURL(path) {
   return `${
-    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
   }${path}`;
 }
 
